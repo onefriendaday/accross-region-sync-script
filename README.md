0402b01d53434ed85c7f8de90fc42675ac1e36c9
Alexander Feiglstorfer <af@storyblok.com>
2020-02-21 05:39:33 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sync script 
+
+Script to sync Storyblok content to different server locations.
+
+Change the space id and provide your oauth tokens in index.js and then execute the script like shown below:
+
+```
+$ npm install
+$ node index.js
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+const Sync = require('./src/sync')
+const StoryblokClient = require('storyblok-js-client')
+const sourceClient = new StoryblokClient({
+  oauthToken: 'SOURCE_OAUTH_TOKEN'
+})
+const targetClient = new StoryblokClient({
+  oauthToken: 'TARGET_OAUTH_TOKEN'
+}, 'https://8xhrfleis8.execute-api.cn-north-1.amazonaws.com.cn/live/v1')
+
+Sync(['Folders', 'Stories'], {
+  sourceSpaceId: 76368,
+  targetSpaceId: 141,
+  sourceClient: sourceClient,
+  targetClient: targetClient
+})
